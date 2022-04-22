c34a43c91d2fee5e0e39b60424e00b6a3e8bd50d
pawellski <skibaapawell@gmail.com>
2022-04-23 01:34:24 +0200

    Added first version of Main class

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.err.println("You should pass path to source code!");
+            System.exit(-1);
+        }
+
+        ANTLRFileStream input = new ANTLRFileStream(args[0]);
+
+        OwnLanguageLexer lexer = new OwnLanguageLexer(input);
+
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        OwnLanguageParser parser = new OwnLanguageParser(tokens);
+
+        ParseTree tree = parser.prog();
+
+        System.out.println(tree.toStringTree(parser));
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+    }
+
+}
