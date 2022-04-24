32ac5fd21576c60fd8aafad24163e0700e7c8508
pawellski <skibaapawell@gmail.com>
2022-04-24 17:45:13 +0200

    Added declare and assign opeartions to LLVM

--- src/LLVMActions.java
+++ src/LLVMActions.java
@@ -12,4 +12,32 @@ public class LLVMActions extends OwnLanguageBaseListener {
         System.out.println(LLVMGenerator.generate());
     }
 
+    @Override
+    public void exitDeclareInt(OwnLanguageParser.DeclareIntContext ctx) {
+        String ID = ctx.ID().getText();
+        variables.put(ID, VarType.INT);
+        LLVMGenerator.declareInt(ID);
+    }
+
+    @Override
+    public void exitDeclareDouble(OwnLanguageParser.DeclareDoubleContext ctx) {
+        String ID = ctx.ID().getText();
+        variables.put(ID, VarType.DOUBLE);
+        LLVMGenerator.declareDouble(ID);
+    }
+
+    @Override
+    public void exitAssign(OwnLanguageParser.AssignContext ctx) {
+        String ID = ctx.ID().getText();
+        Value v = stack.pop();
+        variables.put(ID, v.getType());
+        if (v.getType() == VarType.INT) {
+            LLVMGenerator.declareInt(ID);
+            LLVMGenerator.assignInt(ID, v.getName());
+        } else if (v.getType() == VarType.DOUBLE) {
+            LLVMGenerator.declareDouble(ID);
+            LLVMGenerator.assignDouble(ID, v.getName());
+        }
+    }
+
 }
--- src/LLVMGenerator.java
+++ src/LLVMGenerator.java
@@ -5,7 +5,27 @@ public class LLVMGenerator {
 
     public static String generate() {
         StringBuilder text = new StringBuilder();
+        text.append("\n");
+        text.append(mainText.toString());
         return text.toString();
+    }
+
+    public static void declareInt(String id) {
+        mainText.append("%").append(id).append(" = alloca i32\n");
+    }
+
+    public static void declareDouble(String id) {
+        mainText.append("%").append(id).append(" = alloca double\n");
+    }
+
+   public static void assignInt(String id, String value) {
+       mainText.append("store i32 ").append(value).append(", i32* %")
+        .append(id).append("\n");
+    }
+
+   public static void assignDouble(String id, String value) {
+      mainText.append("store double ").append(value).append(", double* %")
+        .append(id).append("\n");
     }
 
 }
