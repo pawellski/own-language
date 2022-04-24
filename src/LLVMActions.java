d400344f760f9c17788caa57978c2ea2090513c0
pawellski <skibaapawell@gmail.com>
2022-04-25 01:17:18 +0200

    Added casting to int in LLVMActions

--- src/LLVMActions.java
+++ src/LLVMActions.java
@@ -40,6 +40,47 @@ public class LLVMActions extends OwnLanguageBaseListener {
         }
     }
 
+    @Override
+    public void exitInt(OwnLanguageParser.IntContext ctx) {
+        stack.push( new Value(ctx.INT().getText(), VarType.INT));
+    }
+
+    @Override
+    public void exitIntToInt(OwnLanguageParser.IntToIntContext ctx) {
+        String valueName = "%" + LLVMGenerator.getReg();
+        stack.push( new Value(valueName, VarType.INT) );
+    }
+
+	@Override
+    public void exitDoubleToInt(OwnLanguageParser.DoubleToIntContext ctx) {
+        LLVMGenerator.fptosi( ctx.DOUBLE().getText() );
+        String valueName = "%" + LLVMGenerator.getReg();
+        stack.push( new Value(valueName, VarType.INT) );
+    }
+
+    @Override
+    public void exitIdToInt(OwnLanguageParser.IdToIntContext ctx) {
+        String ID = ctx.ID().getText();
+        VarType type = variables.get(ID);
+        if (type != null) {
+            if (type == VarType.INT) {
+                String valueName = "%" + LLVMGenerator.getReg();
+                stack.push( new Value(valueName, VarType.INT) );
+            } else if (type == VarType.DOUBLE) {
+                LLVMGenerator.fptosi(ID);
+                String valueName = "%" + LLVMGenerator.getReg();
+                stack.push( new Value(valueName, VarType.INT) );
+            } else {
+                StringBuilder msg = new StringBuilder();
+                msg.append("mismatch type of variable ").append(ID)
+                    .append(" which is ").append(type);
+                error(ctx.getStart().getLine(), msg.toString());
+            }
+        } else {
+            error(ctx.getStart().getLine(), "undeclared variable " + ID);
+        }
+    }
+
     private void error(int line, String msg){
        System.err.println("Error! Line " + line + ": " + msg);
        System.exit(1);
